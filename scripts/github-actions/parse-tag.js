d03722dd9d66747d4b1045e4516e01f0b3b489a4
Bhavya Subramanian <bhsubra@microsoft.com>
2023-02-07 11:11:42 -0800

    Add documentaion uri

--- scripts/github-actions/parse-tag.js
+++ scripts/github-actions/parse-tag.js
@@ -43,7 +43,7 @@ function parseTag({ core, tag }) {
   core.setOutput("module_path", `${moduleFolder}/${moduleName}`);
   core.setOutput("version", version);
 
-  const readmeLink = `https://github.com/Azure/bicep-registry-modules/tree/${tag}/modules/${tag}/README.md`;
+  const readmeLink = `https://github.com/Azure/bicep-registry-modules/tree/${tag}/modules/${moduleFolder}/README.md`;
   core.setOutput("documentation_uri", readmeLink);
 }
 
